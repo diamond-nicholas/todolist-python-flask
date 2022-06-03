87229ee845ef45b11c0f6c6c76ed6e896834d30c
Diamond Nicholas <diamondnicholas@apples-MacBook-Pro.local>
2022-06-03 16:31:14 +0100

    create model for todo

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_NOTIFICATION'] = False
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+ id = db.Column(db.Integer, primary_key=True)
+ title = db.Column(db.String(100))
+ complete = db.Column(db.Boolean)
+
 @app.route('/')
 def index():
  return render_template('index.html')
